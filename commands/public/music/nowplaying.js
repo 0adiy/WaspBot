41ce5816b2b758da1d560f655dd0abb59f0e8067
0adiy <adityaruva@gmail.com>
2023-07-29 00:37:05 +0530

    added nowplaying embed

--- commands/public/music/nowplaying.js
+++ commands/public/music/nowplaying.js
@@ -2,6 +2,7 @@ import {
   ChatInputCommandInteraction,
   SlashCommandBuilder,
   Client,
+  EmbedBuilder,
 } from "discord.js";
 
 export default {
@@ -14,10 +15,41 @@ export default {
    * @param {Client} client
    */
   async execute(interaction, client) {
+    await interaction.deferReply();
+
     const queue = client.distube.getQueue(interaction);
     if (!queue)
       return interaction.reply(`There is nothing in the queue right now!`);
     const song = queue.songs[0];
-    interaction.reply(`playing **\`${song.name}\`**, by ${song.user}`);
+
+    // Embed
+    const embed = new EmbedBuilder()
+      .setTitle(song.name)
+      .setURL(song.url)
+      .setThumbnail(song.thumbnail)
+      .addFields(
+        {
+          name: "Requested by",
+          value: `${song.user}`,
+          inline: true,
+        },
+        {
+          name: "Duration",
+          value: `${song.formattedDuration}`,
+          inline: true,
+        },
+        {
+          name: "Song by",
+          value: `[${song.uploader?.name}](${song.uploader?.url})`,
+          inline: true,
+        },
+        {
+          name: "Current Time",
+          value: `${queue.currentTime}`,
+          inline: true,
+        }
+      );
+
+    await interaction.editReply({ embeds: [embed] });
   },
 };
--- commands/public/music/play.js
+++ commands/public/music/play.js
@@ -45,20 +45,23 @@ export default {
       const embed = new EmbedBuilder()
         .setTitle(song.name)
         .setURL(song.url)
-        .setDescription(`${song.info}`)
+        // .setDescription(`${song.info}`)
         .setThumbnail(song.thumbnail)
         .addFields(
           {
             name: "Requested by",
             value: `${song.user}`,
+            inline: true,
           },
           {
             name: "Duration",
             value: `${song.formattedDuration}`,
+            inline: true,
           },
           {
             name: "Song by",
             value: `[${song.uploader?.name}](${song.uploader?.url})`,
+            inline: true,
           }
         )
         .setColor(0x0099ff);
